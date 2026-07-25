3d51a2b9647ea8d3ce8a94b2db9aeaa1bf1b65e1
paperplanes <shifankichu200@gmail.com>
2026-07-25 19:44:29 +0530

    hey

--- src/components/layout/BrandLogo.jsx
+++ src/components/layout/BrandLogo.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import amorahLogo from '../../assets/images/amorah-logo-transparent.png';
+import amorahLogo from '../../assets/images/amorah-logo-2nd.jpg';
 
 const sizes = {
   sm: 'w-28 sm:w-32',
